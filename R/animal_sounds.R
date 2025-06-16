f6f35b5f914915cdb66868a03ff81aba6087c841
Carolina Mengoni <caromengoni@hotmail.com>
2025-06-16 06:44:44 -0300

    Documentation for animal sounds function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,16 @@
+#' A Sentence That Describes The Sound An Animal Makes
+#'
+#'
+#'
+#' @param animal A character string
+#' @param sound A character string
+#'
+#' @returns A string sentence containing the parameter values.
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+#' animal_sounds("owl", "hoot")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
